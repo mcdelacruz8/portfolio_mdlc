301aa400044b93b6b36c01b8a4bac15cf7b0cdb4
mcdelacruz8 <88753390+mcdelacruz8@users.noreply.github.com>
2022-11-16 12:54:47 -0800

    Add files via upload

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        My purpose is to help a company, team, or person develop and maintain web applications. (What am i offering to people, my value)
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
